22b76ee986e601273d561b8a2d421f51faf3233e
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-07-19 15:17:42 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# <README Template>
+
+Written by Kris Lafrance
+
+Uses HTML5, Javascript, Bootstrap
+
+# Features
+
+* 
+
+# Run
+
+* `git clone https://github.com/Ero6067/<EXAMPLE>.git`
+* open index.html
---
+++ app.js
@@ -0,0 +1,11 @@
+const PageState = function() {
+  let currentState = new homeState(this);
+
+  this.init = function() {
+    this.change(new homeState);
+  }
+
+  this.change = function(state) {
+    currentState = state;
+  }
+}
